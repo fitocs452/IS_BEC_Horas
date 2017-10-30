fdb3e5f812d3f17370bfe8f32f139af949f780fb
davisalvarez <davisen321@gmail.com>
2017-10-30 10:43:56 -0600

    App Organizer
    Parte del app que administra el organizador

---
+++ organizer/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django. http import HttpResponseRedirect,HttpResponse
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+
+from .models import Organizer
+from .forms import OrganizerModelForm
+# Create your views here.
+def index(request):
+	return HttpResponse("HI This is Organizer App");
+def save(request):
+	if request.method == 'POST':
+		create_form = OrganizerModelForm(request.POST)
+		if create_form.is_valid():
+			create_form.save()
+			messages.add_message(request, messages.SUCCESS, 'Hello world.')
+			return HttpResponseRedirect('../index')
+		else:
+			return render(request,'organizer/save.html',{'organizer_form':create_form})
+	create_form = OrganizerModelForm()
+	return render(request,'organizer/save.html',{'organizer_form':create_form})
